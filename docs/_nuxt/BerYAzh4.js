4bcb511fecd4c5679cea2e0284acdb1792315de9
William Brooks <william.brooks@etu.unilim.fr>
2025-04-28 15:46:42 +0200

    final init

--- docs/_nuxt/BerYAzh4.js
+++
@@ -1,1 +0,0 @@
-import{_ as t,c as o,a,o as s}from"./DMK3arjr.js";const n={};function r(c,e){return s(),o("section",null,e[0]||(e[0]=[a("p",null,"This page will be displayed at the /about route.",-1)]))}const u=t(n,[["render",r]]);export{u as default};
